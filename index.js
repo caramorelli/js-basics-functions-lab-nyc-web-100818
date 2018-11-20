db67c39447e9d95a9062b12dbb924b3a62d9d9f3
Cara Morelli <caramorelli7@gmail.com>
2018-11-20 15:52:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,22 +5,9 @@ function distanceFromHqInBlocks(num) {
 }
 
 function distanceFromHqInFeet(num) {
-  return (Math.max(42, num) -Math.min(42, num)) * 264
+  return (Math.max(42, num) -Math.min(42, num)) * 264;
 }
 
-//   describe('distanceFromHqInFeet()', function() {
-//     it('returns a distance in feet', function() {
-//       expect(distanceFromHqInFeet(43)).to.equal(264);
-//     });
-
-//     it('returns a distance in feet', function() {
-//       expect(distanceFromHqInFeet(50)).to.equal(2112);
-//     });
-
-//     it('calculates distances below 42nd street', function() {
-//       expect(distanceFromHqInFeet(34)).to.equal(2112);
-//     });
-//   });
 
 function distanceTravelledInFeet(num1, num2) {
   return (Math.max(num1, num2) - Math.min(num1, num2)) * 264
