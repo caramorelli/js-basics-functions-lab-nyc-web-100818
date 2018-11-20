660237dfc567e361b3e0f39e52545700b6092121
Cara Morelli <caramorelli7@gmail.com>
2018-11-20 15:45:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ function calculatesFarePrice(start, destination) {
   } else if (num > 2000 && num < 2500) {
     return 25;
   } else {
-    let free = (num - 400)
+    let free = (num - 400);
+    
   }
 }
 
